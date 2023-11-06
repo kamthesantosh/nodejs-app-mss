52b9540a7b69dd586e04994c0ebad22abdd4a2b4
kamthesantosh <117331197+kamthesantosh@users.noreply.github.com>
2023-11-06 19:22:37 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.66.41.88:9000/',
+  serverUrl: http://18.210.20.9:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	  //  'sonar.login': 'squ_16247fd25fd0952e8b6f7149aa6ec718ca5d2f84',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'Staff@123',
+	   'sonar.login': 'squ_76de54d3394b659edccbfbbf54174aec6064aa1e',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'Staff@123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
